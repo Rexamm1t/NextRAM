52f0a6716fd713ec08e58347d2b5ea6412fa4f92
Rexamm1t <rexamm1t@gmail.com>
2025-09-20 20:12:59 +0300

    Обновить kernel_tuning.sh

--- main/kernel_tuning.sh
+++ main/kernel_tuning.sh
@@ -4,15 +4,25 @@ MODDIR=${0%/*}/..
 adjust_swappiness() {
     if [ "$DYNAMIC_SWAPPINESS" = "true" ]; then
         local mem_total=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+        local zram_size=0
+
+        if [ -b "/dev/block/zram0" ]; then
+            zram_size=$(awk '/^\/dev\/block\/zram0/ {print $3}' /proc/swaps)
+        fi
 
         if [ "$mem_total" -lt 2000000 ]; then
-            SWAPPINESS=100
+            SWAPPINESS=150
         elif [ "$mem_total" -lt 4000000 ]; then
-            SWAPPINESS=90
+            SWAPPINESS=100
         else
             SWAPPINESS=80
         fi
 
+        if [ "$zram_size" -gt 0 ]; then
+            SWAPPINESS=$((SWAPPINESS + 20))
+        fi
+
+        SWAPPINESS=$((SWAPPINESS > 180 ? 180 : SWAPPINESS))
         log "INFO" "Dynamic swappiness adjustment: $SWAPPINESS"
     fi
 }
@@ -27,6 +37,10 @@ apply_kernel_tuning() {
         if [ "$PERFORMANCE_MODE" = "true" ]; then
             echo 0 > /proc/sys/vm/oom_kill_allocating_task
             echo 1 > /proc/sys/vm/overcommit_memory
+            echo 100 > /proc/sys/vm/vfs_cache_pressure
+            echo 5 > /proc/sys/vm/dirty_background_ratio
+            echo 20 > /proc/sys/vm/dirty_ratio
+            echo 0 > /proc/sys/vm/laptop_mode
         fi
 
         log "INFO" "Applied kernel tuning parameters"
